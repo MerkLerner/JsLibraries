679b8e774cb4ef1a4471ab0acc1b2d755716978b
student <student@Cyzicus.local>
2017-06-29 15:58:48 -0700

    errythang

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './components/Layout.jsx';
+
+var root = document.getElementById('root');
+ReactDOM.render( <Layout/>, root);
+
